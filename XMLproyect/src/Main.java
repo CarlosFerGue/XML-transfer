9b2a9f6cb352bc434149bdd15003d0ca26fb8383
Carlos Fernández Guevara <131338727+CarlosFerGue@users.noreply.github.com>
2023-11-07 11:28:28 +0100

    sa

--- XMLproyect/src/Main.java
+++ XMLproyect/src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.plaf.nimbus.State;
+import java.io.File;
 import java.sql.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -7,6 +8,10 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         DatabaseConnector databaseConnector = new DatabaseConnector();
+        String xml_exportado = "output.txt";
+        File outputfile = new File(xml_exportado);
+        System.out.println("La ruta del archivo es " + outputfile.getAbsolutePath());
+
         databaseConnector.connectAndRetrieveData();
 
     }
